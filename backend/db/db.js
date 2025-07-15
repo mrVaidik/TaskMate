fe4733e802bce464bded71d536b8bdef110c8757
Vaidikpatil <vaidikpatil0@gmail.com>
2025-07-15 16:12:02 +0530

    env-example

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(
-      "mongodb://localhost:27017/taskmate"
-    );
+    const connection = await mongoose.connect(process.env.DATABASE_URL);
     console.log("Database connected");
   } catch (error) {
     console.log(error);
